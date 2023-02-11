ceb4f5c85fd2aadb6fdaeb72bfba1679b14bd351
Clahryott <ck.truscott@gmail.com>
2023-02-11 12:52:44 +0800

    Update ToDoItem.jsx
    added remove

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -2,15 +2,23 @@ import React from "react";
 
 // CSS
 import "./ToDoItem.css";
-
+// props is an object like a dictionary & is the standard
+// the below if deconstructing and we are are only grabbing what we need
 function ToDoItem(props) {
   const { todo, completeToDo, index } = props;
-
+//object destructuring by removing .todo
+  const {text, isCompleted } = todo
   return (
+  // if todo is complete, else empty string in ""
   <div className={`todo ${toDO.isComplete ? "complete" : ""}`}>
-    {todo.text}
-    <button onClick={() => completeToDo(index)}>Complete</button>
-    <button onClick={() => removeToDo(index)}>Complete</button>
+  {text}
+  {/* todo is being passed */}
+  <div>
+  {/* adds button next to the todoItems */}
+    <button onClick={() => completeToDo(index)}>{`complete${isCompleted ? "d" : ""}`}</button>
+    {/* adds an "x" to remove item */}
+    <button onClick={() => removeToDo(index)}>x</button>
+  </div>
   </div>
   );
 }
